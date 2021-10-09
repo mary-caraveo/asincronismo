3315141af23fa3a9ea10ffe79286f078832dbabb
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-10-09 11:31:00 -0500

    fix: alineación de código

--- src/async/index.js
+++ src/async/index.js
@@ -1,29 +1,29 @@
 const doSomethingAsync = () => {
-    return new Promise((resolve, reject) => {
-        (true)
-            ? setTimeout(() => resolve('Do Something Async'), 3000)
-            : reject(new Error('Test Error'))
-    });
-}
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Do Something Async"), 3000)
+      : reject(new Error("Test Error"));
+  });
+};
 
 const doSomething = async () => {
-    const something = await doSomethingAsync();
-    console.log(something);
-}
+  const something = await doSomethingAsync();
+  console.log(something);
+};
 
-console.log('Before');
+console.log("Before");
 doSomething();
-console.log('After');
+console.log("After");
 
 const anotherFunction = async () => {
-    try {
-        const something = await doSomethingAsync();
-        console.log(something);
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    const something = await doSomethingAsync();
+    console.log(something);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-console.log('Before 1');
+console.log("Before 1");
 anotherFunction();
-console.log('After 1');
+console.log("After 1");
--- src/callback/index.js
+++ src/callback/index.js
@@ -1,26 +1,25 @@
 //Los callbacks son el nombre de una convención para usar funciones que llaman a otras en JavaScript
 function sum(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 function calc(num1, num2, callback) {
-    return callback(num1, num2);
-
+  return callback(num1, num2);
 }
 
 console.log(calc(6, 2, sum));
 
 //
 function date(callback) {
-    console.log(new Date);
-    setTimeout(function () {
-        let date = new Date;
-        callback(date);
-    }, 3000)
+  console.log(new Date());
+  setTimeout(function () {
+    let date = new Date();
+    callback(date);
+  }, 3000);
 }
 
 function printDate(dateNow) {
-    console.log(dateNow);
+  console.log(dateNow);
 }
 
 date(printDate);
